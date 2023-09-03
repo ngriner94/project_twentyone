0f125deece30b55008668d56f4d46c7086072249
unknown <nick.griner@gmail.com>
2023-09-03 17:21:34 -0500

    <changed charlength for age>

--- users/models.py
+++ users/models.py
@@ -3,9 +3,8 @@ from django.db import models
 # Create your models here.
 class user(models.Model):
     Name = models.CharField(max_length=255)
-    Age = models.CharField(max_length=255)
+    Age = models.CharField(max_length=200)
     Hobby = models.CharField(max_length=255)
-
 
 
     
