950ced3511e0cf5275733d2a89ddd89e1dd554d7
unknown <nick.griner@gmail.com>
2023-09-03 17:36:47 -0500

    <changed order of sex class in models.py>

--- users/models.py
+++ users/models.py
@@ -4,10 +4,11 @@ from django.db import models
 class user(models.Model):
     Name = models.CharField(max_length=255)
     Age = models.CharField(max_length=200)
+    Sex = models.CharField(max_length=10)
     Hobby = models.CharField(max_length=255)
     Address = models.CharField(max_length=255)
-    Sex = models.CharField(max_length=10)
-    
+
+
 
 
     
