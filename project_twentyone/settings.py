6e130ac615104697ddbf47f0926977a72c840b71
unknown <nick.griner@gmail.com>
2023-09-04 14:30:58 -0500

    added all allowed hosts

--- project_twentyone/settings.py
+++ project_twentyone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#p41y8ruyt)8r!$o3fj_(3+-vyl5**4kuo1twi(l!=qycn5a%t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
